ce9981790e460d4a963346cde66f2dffd671d1d1
hj-language <idgafff@kumoh.ac.kr>
2021-03-18 00:00:39 +0900

    Koa 기본 세팅

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import 'core-js';
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+});
+
+app.listen(4000, () => {
+    console.log('heurm server is listening to port 4000');
+});
